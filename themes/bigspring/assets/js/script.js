44ae8226a27bf09e2aef83d2212c4ddff4544ba1
cyp t <cyprientaib1@gmail.com>
2021-07-06 19:23:27 +0200

    implementation of dark theme

--- themes/bigspring/assets/js/script.js
+++ themes/bigspring/assets/js/script.js
@@ -1,9 +1,72 @@
+function urlParams(key){
+    let params = null;    
+    let url = window.location.href.split("?")[0]+"?", parameters="", p;
+	if(window.location.href.split("?").length< 2) {return}
+    if ((p = (window.location.href.split("?")[1])).match(key)){
+        for(let k of p.split("&"))
+        {
+            if(k.split("=")[0] != key)
+			{
+                parameters+= "&"+k
+			}
+            else
+			{
+                params = k.split("=")
+			}
+        }
+        window.history.replaceState({}, "Title", url+parameters.substring(1))
+        
+        return params[1]    
+    }
+
+	return null
+}
+
+function verifyDarkTheme(){
+	let urlValue = urlParams("darktheme");
+	let darktheme = localStorage.getItem("darktheme");
+	if(urlValue != null){
+		console.log("Url vlaue")
+		if(urlValue == "toggle"){
+			darktheme = (darktheme!=null && darktheme != "true")
+		} else {
+			darktheme = urlValue
+		}
+		
+		localStorage.setItem("darktheme", darktheme)
+		darktheme = darktheme.toString();
+	}
+	
+	if(darktheme === null){
+		return
+	} else if(darktheme == "false"){
+		document.getElementsByTagName("body")[0].classList.remove("darktheme")
+	}
+		
+
+}
 // Preloader js
 $(window).on('load', function () {
-	$('.preloader').fadeOut(100);
+	$('.preloader').addClass('d-none');
+	verifyDarkTheme()
+
+	document.getElementById("darkthemeToggle")
+		.addEventListener("click", function(){
+		
+			let darktheme = localStorage.getItem("darktheme");
+			darktheme = (darktheme!=null && darktheme != "true")
+			localStorage.setItem("darktheme", darktheme)
+
+			if(darktheme){
+				document.getElementsByTagName("body")[0].classList.remove("darktheme")
+			}else{
+				document.getElementsByTagName("body")[0].classList.add("darktheme")
+			}
+
+	})
 });
 
-$(document).ready(function () {
+(function($) {
 	'use strict';
 
 	//slider
@@ -15,4 +78,4 @@ $(document).ready(function () {
 		arrows: false
 	});
 
-});
+})(jQuery);
